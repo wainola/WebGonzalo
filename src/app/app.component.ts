ecdc1660eaaae199e7921737cd89f06515f98fcf
Nicolás Rafael Riquelme Guzmán <nrriquelme@gmail.com>
2017-02-07 00:20:17 -0300

    Diseño de interfaz del sitio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderSiteComponent } from './header-site.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 }
---
+++ src/app/header-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-site',
+  templateUrl: './header-site.component.html',
+  styles: []
+})
+export class HeaderSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
